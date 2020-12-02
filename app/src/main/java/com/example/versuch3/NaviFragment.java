9aced121a2b7eb89d3c9740d1b1ff68e06d87bd8
arslan <Khmarslan@gmail.com>
2020-12-02 22:10:05 +0100

    Initial commit

---
+++ app/src/main/java/com/example/versuch3/NaviFragment.java
@@ -0,0 +1,29 @@
+package com.example.versuch3;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NaviFragment extends Fragment {
+
+
+    public NaviFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_navi, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/versuch3/NeueFahrtFragment.java
@@ -0,0 +1,28 @@
+package com.example.versuch3;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NeueFahrtFragment extends Fragment {
+
+
+    public NeueFahrtFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_neue_fahrt, container, false);
+    }
+}
